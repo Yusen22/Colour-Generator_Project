7127d5a04a0d86f458471f65abcd6f557c5be572
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-30 21:57:43 +0700

    added state vars and generateShades fn. imported comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import { useState, useEffect } from 'react';
 import { ColourList, Form } from "../Pages";
+import { ToastContainer, toast } from 'react-toastify';
+import Values from 'values.js';
+
+// toast.success('awesome');
+// toast.error('error message');
+
 
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colour, setColour] = useState('');
+  const [shades, setShades] = useState([])
+
+useEffect(() => {
+    console.log(colour);
+}, [colour]);
+
+  const generateShades = (colour) => {
+    const newColours = new Values(colour).all(10)
+    setShades(newColours)
+  }
+  return (
+  <main className='container'>
+    <h2>Color Generator Starter</h2>
+    <Form setColour={setColour} colour={colour} generateShades={generateShades}></Form>
+    <ColourList shades={shades}/>
+    <ToastContainer position='top-center'/>
+  </main>
+
+  )
 };
 export default App;
