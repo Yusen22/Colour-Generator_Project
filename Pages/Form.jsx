d8349d00453e010ce5883e8d8fd9f77959f73ea8
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-30 21:57:00 +0700

    moved state vars to parent and edited evnt listnrs on Form

--- Pages/Form.jsx
+++ Pages/Form.jsx
@@ -1,38 +1,51 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import { useState, useEffect } from "react";
 
-const Form = () => {
-  const [colour, setColour] = useState();
 
+const Form = ({colour, setColour, generateShades}) => {
+  
   const handleChange = (e) => {
-
-  }
+    console.log(e.target.value);
+    setColour(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    generateShades(colour)
   }
+
   return (
-    <form type="submit">
-      <div>
+    <header className="container">
+      <form
+        className="colour-form"
+        onSubmit={handleSubmit}
+      >
         <label htmlFor=""></label>
         <input
+          className="colour-input"
           type="color"
           name="colour"
+          value={colour ? colour : "#47bd14"}
           onChange={handleChange}
         />
-      </div>
-      <div>
-        <label htmlFor=""></label>
-        <input
-          type="text"
-          name="text"
-          onChange={handleChange}
-        />
-        <button
-          type="submit"
-          onSubmit={handleSubmit}
-        ></button>
-      </div>
-    </form>
+        <div className="picker-submit">
+          <label htmlFor=""></label>
+          <input
+            type="text"
+            name="text"
+            onChange={handleChange}
+            value={colour}
+          />
+          <button
+            type="submit"
+            className="form-btn"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </header>
   );
 };
 export default Form;
