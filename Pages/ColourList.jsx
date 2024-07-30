d96da3a1ededb02ff5e41cf924d8afac4f57e75c
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-30 22:55:26 +0700

    added jsx fn to generate colour tiles from shades array

--- Pages/ColourList.jsx
+++ Pages/ColourList.jsx
@@ -1,6 +1,21 @@
-const ColourList = () => {
+import Values from "values.js";
+import { nanoid } from "nanoid";
+
+const ColourList = ({shades}) => {
+  console.log(shades?.[0]?.hex)
   return (
-    <div>ColourList</div>
+    <section>
+      <div className="colour-list">
+        {shades.map((shade) => {
+          const id = nanoid()
+          return (
+            <div className="colour" key={id} style={{backgroundColor: `#${shade.hex}`}}>
+              <h5>#{shade.hex}</h5> 
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 export default ColourList
