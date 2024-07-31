dc30b92887899c7539b44085d02f3f05ae6e3a87
Elijah WIlsher <elijahwilsher@gmail.com>
2024-08-01 01:31:03 +0700

    created Header comp with logo

---
+++ Pages/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+        <h2>Shad<span id="logo-y">y</span></h2>
+        <h4 >The Shade Generator</h4>
+    </header>
+  )
+}
+export default Header
