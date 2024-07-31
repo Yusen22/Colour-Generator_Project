207a816d09a564131ed72c44f0654fab20522f55
Elijah WIlsher <elijahwilsher@gmail.com>
2024-08-01 01:32:46 +0700

    imported Header to index.js

--- Pages/index.jsx
+++ Pages/index.jsx
@@ -1,6 +1,7 @@
 import Form from "./Form";
 import SingleColour from "./SingleColour";
 import ColourList from "./ColourList";
+import Header from "./Header";
 
 
-export {Form, SingleColour, ColourList}
+export {Form, SingleColour, ColourList, Header}
--- README.md
+++ README.md
@@ -89,6 +89,7 @@ async function copyToClipboard(text) {
   try {
     await navigator.clipboard.writeText(text);
     console.log('Text copied to clipboard');
+    toastify
   } catch (error) {
     console.error('Failed to copy text: ', error);
   }
