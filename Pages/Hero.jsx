2e104f67a25c6cd8b084abedc21f1e3f2ed7de98
Elijah WIlsher <elijahwilsher@gmail.com>
2024-08-01 18:44:29 +0700

    created Hero section with animation

---
+++ Pages/Hero.jsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div>
+        <h3></h3>
+        <h5></h5>
+      </div>
+      <div className="animation-container">
+        <div>
+          <div className="loader"></div>
+          <div className="loader-2"></div>
+        </div>
+        <div>
+          <div className="loader-2"></div>
+          <div className="loader"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
