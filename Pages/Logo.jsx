f9cbe5f6c7252a0819408a3396a57960d62bcf5c
Elijah WIlsher <elijahwilsher@gmail.com>
2024-08-01 18:43:37 +0700

    created logo and styles

---
+++ Pages/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Logo = () => {
+  return (
+    <div className="logo ">
+      <h2>
+        Shad<span id="logo-y">y</span>
+      </h2>
+      <h4>The Shade Generator</h4>
+    </div>
+  );
+}
+
+export default Logo
