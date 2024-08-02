68dd291ac4240ff7b77f42c55ffe3602b624e4c5
Elijah WIlsher <elijahwilsher@gmail.com>
2024-08-02 12:41:28 +0700

    created data.js for nav values and hrefs

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        name: 'Home',
+        href: '#',
+        id: 1
+    },
+    {
+        name: 'About',
+        href: '#',
+        id: 2
+    },
+    {
+        name: 'Shades',
+        href: '#',
+        id: 3
+    },
+    {
+        name: 'Contact',
+        href: '#',
+        id: 4
+    },
+]
