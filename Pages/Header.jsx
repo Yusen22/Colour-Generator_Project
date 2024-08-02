55ae82d024b878c965f6a96e7a41820f138046d3
Elijah WIlsher <elijahwilsher@gmail.com>
2024-08-02 12:40:45 +0700

    added nav links to header

--- Pages/Header.jsx
+++ Pages/Header.jsx
@@ -1,8 +1,19 @@
-import Logo  from './Logo';
+import { navLinks } from "../src/data";
+import Logo from "./Logo";
+
 const Header = () => {
   return (
     <header className="header">
       <Logo />
+    <nav className="nav-list">
+          {navLinks.map((link) => {
+            return (
+              <div key={link.id}>
+                <a href={link.href}>{link.name}</a>
+              </div>
+            );
+          })}
+      </nav>
     </header>
   );
 };
